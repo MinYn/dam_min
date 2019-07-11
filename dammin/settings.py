a8b8be62683ff82a8d45c1ea86fb8959eab28874
ysm <ysm@tourbell.co.kr>
2019-07-11 16:32:28 +0900

    static 수정

--- dammin/settings.py
+++ dammin/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "web/templates/static"),
+]
 
 # Media files - 업로드를 하는 URL과 디렉토리 설정
 MEDIA_URL = '/files/' # 업로드 할 경로
