6bd4bd8ce245f50286f8d5d3b8797dead1156d00
Yoon Seok Min <ysmysm3@naver.com>
2019-11-08 23:09:58 +0900

    Heroku 설정 업로드

--- dammin/settings.py
+++ dammin/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import dj_database_url
 
 import os
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'f(*58#xn6gp_cx2nw4ao+2dz8xbo-h2wz3yh1!98_bqd)+0wi+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dammin.herokuapp.com', '.asuscomm.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '.asuscomm.com', '127.0.0.1']
 
 
 # Application definition
@@ -76,11 +77,17 @@ WSGI_APPLICATION = 'dammin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
