96a4441476ed27cea7de8b0be396db21834da2f8
Yoon Seok Min <ysmysm3@naver.com>
2019-11-09 01:11:45 +0900

    Static 파일 업로드

--- dammin/settings.py
+++ dammin/settings.py
@@ -142,9 +142,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 try:
     from .local_settings import *
